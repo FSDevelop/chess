8590166ab241f7ef1d7a7d8d16d84f95a89ab9cf
Federico Sosa <federico.sosa@modelit.xyz>
2016-12-24 07:06:51 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+var server = http.createServer(app);
+var socket = require('socket.io')(server);
+
+socket.on('connection', function(client) {
+    client.on('move', function(movement) {
+        console.log(movement);
+    });
+});
+
+server.listen(8080, function() {
+	console.log('Waiting for players...');
+});
