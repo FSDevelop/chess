5f5d939ad953214ebf16431a64c5248b41bf3194
Federico Sosa <federico.develop@gmail.com>
2016-12-25 23:31:42 -0200

    Added dummy Hipchat response

--- app.js
+++ app.js
@@ -20,16 +20,36 @@ fs.readFile('default.json', 'utf8', function (err, data) {
         });
         
         client.on('requestInit', function(roomId) {
+            if (pieces[roomId] == null) {
+                pieces[roomId] = JSON.parse(data);
+            }
             client.emit('init', JSON.stringify(pieces[roomId]));
         });
         
         client.on('placePiece', function(data) {
-            console.log('placePiece');
             pieces[data.roomId][data.pieceMoved.index] = data.pieceMoved.piece;
             if (data.pieceEaten != null) {
                 pieces[data.roomId][data.pieceEaten].eaten = true;
             }
             client.broadcast.emit('updatePieces', JSON.stringify(pieces[data.roomId]));
+        });
+    });
+});
+
+app.listen(3000, function() {
+    console.log('Express in 3000...');
+});
+
+app.get('/generateRoom', function(res, req) {
+    fs.readFile('default.json', 'utf8', function (err, data) {
+        var roomId = Math.floor(Math.random() * 10000);
+        pieces[roomId] = JSON.parse(data);
+        
+        req.send({
+            "color": "green",
+            "message": "It's going to be sunny tomorrow! (yey)",
+            "notify": false,
+            "message_format": "text"
         });
     });
 });
